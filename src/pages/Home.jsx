a2c46504d65087f61cf6a60ad25b3c43c8d351f2
Hendy Setiawan <hendystyawan74@gmail.com>
2022-08-19 20:03:52 +0700

    create home pages and popular list component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PopularRecipeList from '../components/PopularRecipeList';
+
+function Home() {
+  return (
+    <div>
+      <PopularRecipeList />
+    </div>
+  );
+}
+
+export default Home;
