e82c54d5daf4465936bc15f2ca669368802dfcc0
Hendy Setiawan <hendystyawan74@gmail.com>
2022-08-19 19:56:08 +0700

    create recipe service api

---
+++ src/services/recipe-services.js
@@ -0,0 +1,17 @@
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const getPopularRecipe = async () => {
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}?number=8`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getPopularRecipe };
