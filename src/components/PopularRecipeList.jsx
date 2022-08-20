d284bf56b5114b8eb627bfd560b62e0864eb4ec8
Hendy Setiawan <hendystyawan74@gmail.com>
2022-08-20 18:04:15 +0700

    create popular recipe list with slider

--- src/components/PopularRecipeList.jsx
+++ src/components/PopularRecipeList.jsx
@@ -1,7 +1,91 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { getPopularRecipe } from '../services/recipe-services';
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 function PopularRecipeList() {
-  return <div>PopularRecipeList</div>;
+  const [popularRecipes, setPopularRecipes] = useState([]);
+  const popularRecipeHandler = async () => {
+    const recipe = await getPopularRecipe();
+    setPopularRecipes(recipe.recipes);
+  };
+  useEffect(() => {
+    popularRecipeHandler();
+  }, []);
+  return (
+    <div>
+      <Wraper>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: true,
+            pagination: false,
+            drag: 'free',
+            gap: '2rem',
+          }}
+        >
+          {popularRecipes.map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card key={recipe.id}>
+                  <h3>{recipe.title}</h3>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wraper>
+    </div>
+  );
 }
+
+const Wraper = styled.section`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  height: 15rem;
+  overflow: hidden;
+  position: relative;
+  border-radius: 2rem;
+  text-align: text-center;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    display:block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  h3 {
+    position: absolute;
+    z-index: 10;
+    bottom: 0%;
+    color: white;
+    text-align: text-center;
+    width: 100%;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    heigth: 40%;
+  }
+`;
+
+const Gradient = styled.div`
+  position: absolute;
+  z-index: 5;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 
 export default PopularRecipeList;
--- src/services/recipe-services.js
+++ src/services/recipe-services.js
@@ -3,7 +3,7 @@ const apiKey = process.env.REACT_APP_API_KEY;
 const getPopularRecipe = async () => {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}?number=8`
+      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=8`
     );
     const data = await response.json();
     console.log(data);
@@ -14,4 +14,4 @@ const getPopularRecipe = async () => {
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getPopularRecipe };
+export { getPopularRecipe };
