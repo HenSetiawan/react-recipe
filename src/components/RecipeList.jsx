dbba261d1359d05a535a870da911336d4cfcd1a3
Hendy Setiawan <hendystyawan74@gmail.com>
2022-08-25 15:14:59 +0700

    create recipe list

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { getPopularRecipe } from '../services/recipe-services';
 import styled from 'styled-components';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
 
 function PopularRecipeList() {
   const [popularRecipes, setPopularRecipes] = useState([]);
@@ -17,62 +15,51 @@ function PopularRecipeList() {
   return (
     <div>
       <Wraper>
-        <Splide
-          options={{
-            perPage: 4,
-            arrows: true,
-            pagination: false,
-            drag: 'free',
-            gap: '2rem',
-          }}
-        >
-          {popularRecipes.map((recipe) => {
-            return (
-              <SplideSlide>
-                <Card key={recipe.id}>
-                  <h3>{recipe.title}</h3>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
-                </Card>
-              </SplideSlide>
-            );
-          })}
-        </Splide>
+        {popularRecipes.map((recipe) => {
+          return (
+            <Card key={recipe.id}>
+              <Gradient />
+              <h3>{recipe.title}</h3>
+              <img src={recipe.image} alt={recipe.title} />
+            </Card>
+          );
+        })}
       </Wraper>
     </div>
   );
 }
 
 const Wraper = styled.section`
-  margin:5rem auto;
+  margin: 5rem auto;
   width: 80%;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  text-align:center;
   font-family: 'Montserrat', sans-serif;
 `;
 
 const Card = styled.div`
-  height: 15rem;
-  overflow: hidden;
+  width: 21%;
   position: relative;
+  margin-top: 10px;
   border-radius: 1.5rem;
-  text-align:center;
-
+  
   img {
-    border-radius: 1.5rem;
-    position: absolute;
-    left: 0;
-    display: block;
     width: 100%;
-    height: 100%;
-    object-fit: cover;
+    display: block;
+    border-radius: 1.5rem;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
   }
 
   h3 {
     position: absolute;
     z-index: 10;
-    bottom: 0%;
     color: white;
+    bottom: 0%;
     font-weight: 400;
-    font-size:16px;
+    font-size: 16px;
     left: 0;
     width: 100%;
     text-align:center;
@@ -83,8 +70,8 @@ const Gradient = styled.div`
   position: absolute;
   z-index: 5;
   width: 100%;
+  border-radius: 1.5rem;
   height: 100%;
   background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
-
 export default PopularRecipeList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PopularRecipeList from '../components/PopularRecipeList';
+import RecipeList from '../components/RecipeList';
 
 function Home() {
   return (
     <div>
       <PopularRecipeList />
+      <RecipeList />
     </div>
   );
 }
